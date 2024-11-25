434de25391b30741d33c1666dc254f0e4dfeca22
AndriySydorchenko <a.sydorchenko@finik.pro>
2024-11-25 20:40:39 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '/password-strength-checker',
+    // basePath: '/password-strength-checker',
     reactStrictMode: true,
 };
 
